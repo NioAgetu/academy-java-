5cf88931a69b9c62bf31b33873ac8b382efb7efa
NioAgetu <onyinyeagetu@gmail.com>
2024-10-04 02:26:03 -0400

    thursday class code

---
+++ src/main/java/com/bptn/course/_200_CodioPractice/_02_family_tree_teachback/FamilyTree.java
@@ -0,0 +1,61 @@
+package com.bptn.course._200_CodioPractice._02_family_tree_teachback;
+
+public class FamilyTree {
+	
+	private String name;
+	private String birthday;
+	private int age;
+	
+	public FamilyTree (String name, String birthday, int age) {
+		this.name = name;
+		this.birthday = birthday;
+		this.age =  age;
+	}
+	
+	public String toString() {
+		return "The family name is " + name + ", birthday is: " + birthday + " and age is " + age;
+	}
+	
+	
+	
+	public int hashCode()
+	{
+		return 34567890;
+	}
+	
+	
+	public int nextBirthday() {
+		return age + 1;
+	}
+	
+	public String relativeType() {
+		return "Family Member";
+	}
+
+	public static void main(String[] args) {
+		FamilyTree ft1 = new FamilyTree ("Webster", "june 10 1967", 57);
+		FamilyTree ft2 = new FamilyTree ("Kodi", "october 13 2007", 17);
+		
+		String printToString = ft1.toString();
+		System.out.println(printToString);
+		
+		ft2.getClass();
+		System.out.println(ft2.getClass());
+		
+		ft2.hashCode();
+		System.out.println(ft2.hashCode());
+		
+		ft2.nextBirthday();
+		System.out.println(ft2.nextBirthday());
+		
+		System.out.println(ft2.relativeType());
+		
+	
+		/*public Class getClass() {
+		return "Teh family name is " + name + ", birthday is: " + birthday + " and age is " + age;
+	}*/
+	}
+	
+	
+
+}
---
+++ src/main/java/com/bptn/course/_200_CodioPractice/_02_family_tree_teachback/Sister.java
@@ -0,0 +1,26 @@
+package com.bptn.course._200_CodioPractice._02_family_tree_teachback;
+
+public class Sister extends FamilyTree {
+	private String personalityType;
+
+	public Sister(String name, String birthday, int age, String personalityType ) {
+		super(name, birthday, age);
+		this.personalityType = personalityType;
+		
+	}
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sister sissy = new Sister ("lisa", "82 July 1998", 36, "fun");
+		
+		System.out.println(sissy.nextBirthday());
+		
+
+	}
+
+}
